1c4945eef7fa24e5feb9dd3ce0e00b5c937fc71b
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2025-02-11 01:12:12 -0300

    add lib navigation and serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
 android {
@@ -40,6 +41,12 @@ android {
 }
 
 dependencies {
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Kotlin Serialization
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
